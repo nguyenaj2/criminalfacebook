468f8d7cec33919a38d8cec76e4c5b8755e0e16f
Jeffrey Nguyen <nguyenaj2@vcu.edu>
2018-11-29 18:30:20 -0500

    Add files via upload

---
+++ victimaddress.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+    width: 100%;
+    border-collapse: collapse;
+}
+
+table, td, th {
+    border: 1px solid black;
+    padding: 5px;
+}
+
+th {text-align: left;}
+</style>
+</head>
+<body>
+
+<?php
+$t = $_POST['ssn'];
+
+$servername = "127.0.0.1";
+$username = "user";
+$password = "password"
+$dbname = "criminal_records";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, null, '/cloudsql/criminal-facebook:us-central1:criminal-database');
+
+// Check connection
+if ($conn->connect_error) {
+    echo 'Failed to connect';
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+$query = "select address from person natural join victim where ssn = $t";
+$result = mysqli_query($conn,$query);
+
+
+echo "<table border='1'>
+<tr>
+<th>Address</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    echo "<td>" . $row['address'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+$conn->close();
+?>
+</body>
+</html>
